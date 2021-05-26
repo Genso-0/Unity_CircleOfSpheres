400940cdf63573496f3820acb78889771b07d912
Genso-0 <44528817+Genso-0@users.noreply.github.com>
2021-05-26 08:04:02 +0200

    Create CircleOfSpheres.cs

---
+++ CircleOfSpheres.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CircleOfSpheres : MonoBehaviour
+{ 
+    public float pointSize = .1f;
+    public int totalPoints;
+    public const float circleInRadians = 6.28319f;
+    void OnDrawGizmos()
+    { 
+        var segmentWidth = GetSegmentWidth(totalPoints);
+        var radius = GetRadius(totalPoints, pointSize);
+        Gizmos.DrawSphere(transform.position, 1);
+        float angle = 0;
+        for (int i = 0; i < totalPoints; i++)
+        { 
+            float x = radius * Mathf.Cos(angle);
+            float y = radius * Mathf.Sin(angle);
+            angle += segmentWidth;
+            var pos = new Vector3(x, 0, y) + transform.position;
+
+            Gizmos.DrawSphere(pos, pointSize); 
+        }
+    } 
+    private float GetRadius(int totalPoints, float nodeSize)
+    {
+        //From the formula LengthOfCircle = radius * theta; -> radius = lengthOfCircle / theta -> radius = (totalPoints * pointSize)/360degrees 
+        return (totalPoints * (nodeSize * 2)) / circleInRadians; 
+    } 
+    private float GetSegmentWidth(int count)
+    {
+        return Mathf.PI * 2f / count;
+    }
+}
